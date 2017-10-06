ba3f8e89c2492f4132db91c7d771e6537d4eb206
MikhailPakaliuk <mr.dyurilen@mail.ru>
2017-10-06 10:58:01 +0300

    Counting zeros
    Implemented the counting function of zeros at the end of the line

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function zeros(expression) {
-  // your solution
+  function countZeros(value) {
+  let strZeros = '0';
+  while (value.endsWith(strZeros)) {
+    strZeros = 0 + strZeros;
+  }
+  return strZeros.length - 1;
+}
+  return 0;
 }
